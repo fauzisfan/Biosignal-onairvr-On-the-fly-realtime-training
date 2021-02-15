46ae7718ab14f9758b6e806f2073a2bf98d6bd25
Isfan Fauzi <68592551+fauzisfan@users.noreply.github.com>
2021-02-15 14:49:58 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -8,14 +8,18 @@ import quaternion
 import tensorflow as tf
 from realPred_train import pred
 from train import train_nn
-# from realPred import pred
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 class App(PredictModule):
+    
     def parse_command_args(self):
-        port = feedback = input_file = output = metric_output = None
+        port = feedback = input_file = output = metric_output = game_event_output = None
+        accept_client_buttons = False
         
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "p:f:m:o:i:")
+            opts, _args = getopt.getopt(sys.argv[1:], "p:f:m:o:i:g:", ["accept-client-buttons"])
         except getopt.GetoptError as err:
             print(err)
             sys.exit(1)
@@ -31,43 +35,40 @@ class App(PredictModule):
                 output = arg
             elif opt == "-m":
                 metric_output = arg
+            elif opt == "-g":
+                game_event_output = arg
+            elif opt == "--accept-client-buttons":
+                accept_client_buttons = True
             else:
                 assert False, "unhandled option"
                 
-        return port, feedback, input_file, output, metric_output
+        return port, feedback, input_file, output, metric_output, game_event_output, accept_client_buttons
 
     def run(self):
-		
         self.rt_counter = 0
-        self.x_seq_rt = np.zeros(shape=(6, 9))
+        self.x_seq_rt = np.zeros(shape=(7, 9))
         self.timestamp_rt = np.zeros(shape=(2, 1), dtype= float)
         self.gyro_rt = np.zeros(shape=(2, 3), dtype= float)
         self.eul_rt = np.zeros(shape=(2, 3), dtype= float)
 
         self.flag = 0
-        self.trainTime = 2*3600 #number of minutes
+        self.trainTime = 2*3600 #Number of minutes
         self.train_counter = 0
         self.eul_train = np.zeros((0,3), dtype= float)
         self.gyro_train = np.zeros((0,3), dtype= float)
         self.time_stamp = np.zeros((0,1), dtype= float)
         self.input_nm = 9
-#		
-        port_input, port_feedback, input_file, output, metric_output = \
+        
+        port_input, port_feedback, input_file, output, metric_output, game_event_output, accept_client_buttons = \
             self.parse_command_args()
         if input_file is None:
             assert(port_input is not None and port_feedback is not None)
-
+            
             server = MotionPredictServer(
-                self, port_input, port_feedback, output, metric_output
+                self, port_input, port_feedback, output, metric_output, game_event_output, accept_client_buttons
             )
 
-            try:
-                server.run()
-                
-            except KeyboardInterrupt:
-                pass
-            finally:                
-                server.shutdown()
+            server.run()
                 
         else:
             assert(output is not None)
@@ -78,7 +79,8 @@ class App(PredictModule):
                 simulator.run()
             except KeyboardInterrupt:
                 pass
-        
+    
+    # implements PredictModule
     def quat_to_euler(self, quat):        
         siny_cosp = 2 * (quat[3] * quat[1] - quat[2] * quat[0])
         cosy_cosp = 1 - 2 * (quat[1] * quat[1] + quat[2] * quat[2])
@@ -112,10 +114,11 @@ class App(PredictModule):
         quat = np.nan * np.ones( (eul.shape[0],4) )
         quat[:,0] = cos_pitch * cos_yaw * cos_roll + sin_pitch * sin_yaw * sin_roll
         quat[:,1] = cos_pitch * cos_yaw * sin_roll - sin_pitch * sin_yaw * cos_roll
-        quat[:,2] = cos_pitch * sin_yaw * cos_roll + sin_pitch * cos_yaw * sin_roll 
-        quat[:,3] = sin_pitch * cos_yaw * cos_roll - cos_pitch * sin_yaw * sin_roll     
+        quat[:,2] = cos_pitch * sin_yaw * cos_roll + sin_pitch * cos_yaw * sin_roll
+        quat[:,3] = sin_pitch * cos_yaw * cos_roll - cos_pitch * sin_yaw * sin_roll
         return (quat[:,[3,1,2,0]])
         
+    
     def Robust_overfilling(self, input_orientation, prediction, input_projection, offset = 1, fixed_param = 1):
         #Get Input Projection Distance for each side in x,y coordinate
         IPDx = input_projection[2]-input_projection[0]
@@ -133,9 +136,9 @@ class App(PredictModule):
         input_angley = np.arctan(IPDx/(2*h))
     	
     	#Get user's direction based on prediction motion
-        pitch_diff = (prediction[0]-input_orientation[0])*np.pi/180
-        roll_diff = (prediction[1]-input_orientation[1])*np.pi/180
-        yaw_diff = (prediction[2]-input_orientation[2])*np.pi/180
+        pitch_diff = (prediction[0][0]-input_orientation[0])*np.pi/180
+        roll_diff = (prediction[0][1]-input_orientation[1])*np.pi/180
+        yaw_diff = (prediction[0][2]-input_orientation[2])*np.pi/180
     	
     	#Calculate predicted margin based on translation movement
         x_r = max(input_projection[2],rx*abs(np.sin(input_anglex-yaw_diff)))
@@ -171,10 +174,10 @@ class App(PredictModule):
     	
     	#Calculate margin based on genrated area
         margin = np.sqrt((p_r-p_l)*(p_t-p_b)*offset)/2
-        p_l = -(margin+np.sin(yaw_diff))
-        p_t = margin+np.sin(pitch_diff)
-        p_r = margin-np.sin(yaw_diff)
-        p_b = -(margin-np.sin(pitch_diff))
+        p_l = -(margin+np.sin(yaw_diff))+(input_projection[0]+1)
+        p_t = margin+np.sin(pitch_diff)+(input_projection[1]-1)
+        p_r = margin-np.sin(yaw_diff)+(input_projection[2]-1)
+        p_b = -(margin-np.sin(pitch_diff))+(input_projection[3]+1)
     	
         'Enhancement ver 2'
     ##	Get dilation on high velocity
@@ -190,6 +193,7 @@ class App(PredictModule):
         return [-np.abs(p_l),np.abs(p_t),np.abs(p_r),-np.abs(p_b)]
         # margins = np.max(np.abs([p_l, p_t, p_r, p_b]))
         # return [-margins, margins, margins, -margins]
+    
     
     def calc_optimal_overhead(self, hmd_orientation, frame_orientation, hmd_projection):
         q_d = np.matmul(
@@ -225,41 +229,46 @@ class App(PredictModule):
     # 	a_hmd = (hmd_projection[2] - hmd_projection[0]) * (hmd_projection[1] - hmd_projection[3])
         
         return [-np.abs(p_l),np.abs(p_t),np.abs(p_r),-np.abs(p_b)]
-	
-	# implements PredictModule
-    '''Prediction with training'''
+        # margins = np.max(np.abs([p_l, p_t, p_r, p_b]))
+        # return [-margins, margins, margins, -margins]
+
     def predict(self, motion_data, sess):
         # no prediction
         prediction_time = 300.0  # ms
-		
-        input_euler = self.quat_to_euler(motion_data.orientation)#self.get_euler()
+        # print(motion_data.head_orientation)
+        
+        input_euler = self.quat_to_euler(motion_data.head_orientation)
         input_euler = np.array([input_euler[1],input_euler[2],input_euler[0]])*180/np.pi
-        self.input_gyro = np.array(motion_data.angular_velocities)*180/np.pi
+        self.input_gyro = np.array(motion_data.head_angular_velocity)*180/np.pi
         input_time = motion_data.timestamp/ 705600000
+        # self.input_gyro = (input_euler/input_time).reshape(1,-1) * (np.pi/180)
+        # print('HI...=====================================')
         
         if (self.flag == 0):
-            predicted_orientation = motion_data.orientation
+            predicted_head_orientation = motion_data.head_orientation
             self.overfilling = motion_data.camera_projection
             ann = cap = crp = np.array(input_euler).reshape(1,-1)
         
-        if (self.flag == 0 and self.train_counter >= 1500):
+        if (self.flag == 0 and self.train_counter >= 1300):
             self.eul_train = np.concatenate((self.eul_train, np.array(input_euler).reshape(1,-1)), axis =0)
             self.gyro_train = np.concatenate((self.gyro_train, np.array(self.input_gyro).reshape(1,-1)), axis =0)
-            self.time_stamp = np.concatenate((self.time_stamp, np.array(input_time).reshape(1,-1)), axis =0)            
+            self.time_stamp = np.concatenate((self.time_stamp, np.array(input_time).reshape(1,-1)), axis =0)
             print(self.train_counter)
-            if (self.train_counter == 1500+self.trainTime):
-                self.flag, self.model, self.DELAY_SIZE, self.midVel, self.avgVel = train_nn(self.eul_train, self.gyro_train, self.time_stamp)#trainInit.get()
-#                print(self.midVel)
-#                print(self.avgVel)
+            if (self.train_counter == 1300+self.trainTime):
+                self.flag, self.model, self.DELAY_SIZE, self.midVel, self.avgVel = train_nn(self.eul_train, self.gyro_train, self.time_stamp)
+                # print(self.midVel)
+                # print(self.avgVel)
                 self.x = tf.placeholder(dtype=tf.float32, shape=(None, self.DELAY_SIZE, self.input_nm))
                 self.y = self.model(self.x)
-            
         elif (self.flag == 1):# get the data after 17 seconds (60 FPS is assumed)
             # Overhead Orientation
             idx = [0,3,1,2]
+            # hmd_orientation = quat_quat_data[:,idx]
+            # frame_orientation = quat_predict[:,idx]
             
             ann, cap, crp, nop, self.x_seq_rt = pred(input_euler, self.input_gyro, input_time, self.x_seq_rt, self.timestamp_rt, self.eul_rt, self.gyro_rt, self.rt_counter, sess, self.model, self.DELAY_SIZE, self.y, self.x, self.midVel, self.avgVel)
-
+            # print(ann)
+            # print(input_euler)
             quat_quat_predict = self.euler_to_quat(ann)
             quat_quat_data = self.euler_to_quat(input_euler.reshape(1,-1))
             projection_input = motion_data.camera_projection
@@ -267,7 +276,7 @@ class App(PredictModule):
             # hmd_orientation = quat_quat_data[:,idx]
             # frame_orientation = quat_quat_predict[:,idx]
             
-            # #Calculate overfiling
+            #Calculate overfiling
             # input_orientation = np.quaternion(
             #     hmd_orientation[:,0],
             #     hmd_orientation[:,1],
@@ -280,14 +289,15 @@ class App(PredictModule):
             #     frame_orientation[:,2],
             #     frame_orientation[:,3]
             #     )
-            input_projection = projection_input
+            
+            if (self.rt_counter<self.DELAY_SIZE):
+                margin = projection_input
+            else:
+                '''Optimal Overfilling'''
+                # margin = self.calc_optimal_overhead(input_orientation, predict_orientation, projection_input)
+                '''Model-Based Overfilling'''
+                margin = self.Robust_overfilling(input_euler, ann, projection_input, offset = 1.1, fixed_param = 1.3)
 
-            # margin = self.calc_optimal_overhead(input_orientation, predict_orientation, input_projection)
-            margin = self.Robust_overfilling(input_euler, ann[0], input_projection, offset = 1.1, fixed_param = 1.3)
-#            margin[0] = margin[0]+motion_data.camera_projection[0]
-#            margin[1] = margin[1]-motion_data.camera_projection[1]
-#            margin[2] = margin[2]-motion_data.camera_projection[2]
-#            margin[3] = margin[3]+motion_data.camera_projection[3]
             
             print('\nInput [Pitch, Roll, Yaw]: {:.2f}, {:.2f}, {:.2f}'.format(input_euler[0],input_euler[1],input_euler[2]))
             print('ANN [Pitch, Roll, Yaw]: {:.2f}, {:.2f}, {:.2f}'.format(ann[0][0],ann[0][1],ann[0][2]))
@@ -296,88 +306,34 @@ class App(PredictModule):
             
             self.rt_counter += 1
             
-            predicted_orientation = quat_quat_predict[0]
-            # predicted_orientation = quat_quat_data[0]
-            # predicted_orientation = motion_data.orientation
+            # predicted_head_orientation = quat_quat_predict[0]
+            # predicted_head_orientation = quat_quat_data[0]
+            predicted_head_orientation = motion_data.head_orientation
             
             # print(quat_quat_predict[0])
             # print(quat_quat_data[0])
-            # print(motion_data.orientation)
-            
+            # print(motion_data.head_orientation)
+        
         self.train_counter += 1
-        # overfilling delta in radian (left, top, right, bottom)
-        projection_output = self.overfilling
-
-        return prediction_time, predicted_orientation, projection_output, input_euler, ann, cap, crp, self.flag
-
-    '''Prediction without training'''
-#    def predict(self, motion_data, sess):
-#         # no prediction
+        predicted_left_eye_pos = motion_data.left_eye_position
+        predicted_right_eye_pos = motion_data.right_eye_position
         
-#         prediction_time = 300.0  # ms
-# 		
-#         input_euler = self.quat_to_euler(motion_data.orientation)#self.get_euler()
-#         input_euler = np.array([input_euler[1],input_euler[2],input_euler[0]])*180/np.pi
-#         input_gyro = np.array(motion_data.angular_velocities)*180/np.pi
-#         input_time = motion_data.timestamp/ 705600000
-        
-#         if (self.flag == 0):
-#             predicted_orientation = motion_data.orientation
-#             self.overfilling = motion_data.camera_projection
-#             ann = cap = crp = np.array(input_euler).reshape(1,-1)
 
-#         if (self.rt_counter >= 1500):
-#             self.flag = 1
-#             idx = [0,3,1,2]
-            
-#             ann, cap, crp, nop, self.x_seq_rt = pred(input_euler, input_gyro, input_time, self.x_seq_rt, self.timestamp_rt,  self.gyro_rt, self.rt_counter, sess)
-#             quat_quat_predict = self.euler_to_quat(ann)
-#             quat_quat_data = self.euler_to_quat(input_euler.reshape(1,-1))
-#             projection_input = motion_data.camera_projection
-            
-#             hmd_orientation = quat_quat_data[:,idx]
-#             frame_orientation = quat_quat_predict[:,idx]
-            
-#             #Calculate overfiling
-#             input_orientation = np.quaternion(
-#                 hmd_orientation[:,0],
-#                 hmd_orientation[:,1],
-#                 hmd_orientation[:,2],
-#                 hmd_orientation[:,3]
-#                 )
-#             predict_orientation = np.quaternion(
-#                 frame_orientation[:,0],
-#                 frame_orientation[:,1],
-#                 frame_orientation[:,2],
-#                 frame_orientation[:,3]
-#                 )
-#             input_projection = projection_input
-    
-#             margin = self.calc_optimal_overhead(input_orientation, predict_orientation, input_projection)
-#             # margin = self.Robust_overfilling(input_euler, ann, input_projection, offset = 1, fixed_param = 1)
-#     #            margin[0] = margin[0]+motion_data.camera_projection[0]
-#     #            margin[1] = margin[1]-motion_data.camera_projection[1]
-#     #            margin[2] = margin[2]-motion_data.camera_projection[2]
-#     #            margin[3] = margin[3]+motion_data.camera_projection[3]
-            
-#             print('\nInput [Pitch, Roll, Yaw]: {:.2f}, {:.2f}, {:.2f}'.format(input_euler[0],input_euler[1],input_euler[2]))
-#             print('ANN [Pitch, Roll, Yaw]: {:.2f}, {:.2f}, {:.2f}'.format(ann[0][0],ann[0][1],ann[0][2]))
-#             print('Overfilling: {:.2f},{:.2f},{:.2f},{:.2f}'.format(margin[0],margin[1], margin[2], margin[3]))
-#             self.overfilling = [margin[0], margin[1], margin[2], margin[3]]
-            
-#             predicted_orientation = quat_quat_predict[0]
-#             # predicted_orientation = quat_quat_data[0]
-#             # predicted_orientation = motion_data.orientation
+        # predicted_camera_projection = self.make_camera_projection(motion_data, self.overfilling)
         
-#         self.rt_counter += 1
+        predicted_camera_projection = self.overfilling
         
-#         # print(quat_quat_predict[0])
-#         # print(quat_quat_data[0])
+        predicted_right_hand_pos = motion_data.right_hand_position
+        predicted_right_hand_ori = motion_data.right_hand_orientation
 
-#         # overfilling delta in radian (left, top, right, bottom)
-#         projection_output = self.overfilling
-
-#         return prediction_time, predicted_orientation, projection_output, input_euler, ann, cap, crp, self.flag
+        return prediction_time, \
+               predicted_left_eye_pos, \
+               predicted_right_eye_pos, \
+               predicted_head_orientation, \
+               predicted_camera_projection, \
+               predicted_right_hand_pos, \
+               predicted_right_hand_ori, \
+               input_euler, ann, cap, crp, self.flag
 
     def feedback_received(self, feedback):
         # see PrefMetricWriter.write_metric() to understand feedback values
@@ -386,18 +342,20 @@ class App(PredictModule):
         # example : calculate overall latency
         #
         # overall_latency = feedback['endClientRender'] - feedback['gatherInput']
-        
+        pass
+
+    def external_input_received(self, input_data):
+        #print(input_data, flush=True)
         pass
     
+    def game_event_received(self, event):
+        print(event, flush=True)
+        pass
     
 def main():
     app = App()
     app.run()
 
-	
-def ambil_euler(MotionPredictServer):
-	nilai_euler = MotionPredictServer.get_euler
-	return nilai_euler
-
+    
 if __name__ == "__main__":
     main()
